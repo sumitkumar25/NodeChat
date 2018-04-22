1042583bba52d795a550c0ba45bf5b944b84dfff
sumit <sumitchauhancoe@gmail.com>
2018-04-22 18:04:19 +0530

    socket setup

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-const app = express();
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log(socket);
+});
+server.listen(port, () => {
     console.log('started at port ' + port);
 });
