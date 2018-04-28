c4ab4762b0d8290636a79849055da8cee4c9bc86
sumit <sumitchauhancoe@gmail.com>
2018-04-29 00:58:48 +0530

    test cases for message util

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,8 +1,21 @@
 var expect = require('expect');
-var { generateMessage } = require('./message');
+var { generateMessage, generateLocationMessage } = require('./message');
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
-        let res = generateMessage('testUser', 'testMessage');
-        expect(res).toInclude({ from, text });
+        let from = 'testUser';
+        let text = 'testMessage';
+        let res = generateMessage(from, text);
+        expect(res).toMatchObject({ from, text });
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct message object', () => {
+        var from = 'test';
+        var lat = 10;
+        var long = 20;
+        var url = 'https://www.google.com/maps?q=10,20'
+        let res = generateLocationMessage(from, lat, long);
+        expect(res).toMatchObject({ from, url });
     });
 });
